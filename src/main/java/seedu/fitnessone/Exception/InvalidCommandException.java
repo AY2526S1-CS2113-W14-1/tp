944db14ecb693aa6168a5f52cd7c1e32a852891f
Izen Toh <E1406600@u.nus.edu>
2025-10-01 14:44:03 +0800

    Revert "Setup base CLI code for #8"

--- src/main/java/seedu/fitnessone/Exception/InvalidCommandException.java
+++
@@ -1,8 +0,0 @@
-package seedu.fitnessone.Exception;
-
-public class InvalidCommandException extends Throwable {
-    public InvalidCommandException(String message) {
-        super(message);
-    }
-
-}
