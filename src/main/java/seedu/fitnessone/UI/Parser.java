8cd3d2289ff775c769211558485fc371b64b0538
Izen9835 <e1406600@u.nus.edu>
2025-10-01 15:56:43 +0800

    Setup base CLI code for #8 second attempt

--- src/main/java/seedu/fitnessone/UI/Parser.java
+++ src/main/java/seedu/fitnessone/UI/Parser.java
@@ -1,7 +1,6 @@
 package seedu.fitnessone.UI;
 
-import seedu.fitnessone.Command.Command;
-import seedu.fitnessone.Command.ExitCommand;
+import seedu.fitnessone.Command.*;
 import seedu.fitnessone.Exception.InvalidCommandException;
 
 public class Parser {
@@ -21,6 +20,15 @@ public class Parser {
         switch (commandWord) {
             case "bye":
                 return new ExitCommand();
+
+            case "list":
+                return new ListAthleteCommand();
+
+            case "addAthlete":
+                return new AddAthleteCommand(trimmedInput);
+
+            case "delAthlete":
+                return new DeleteAthleteCommand(trimmedInput);
 
             default:
                 throw new InvalidCommandException("input keyword not found");
