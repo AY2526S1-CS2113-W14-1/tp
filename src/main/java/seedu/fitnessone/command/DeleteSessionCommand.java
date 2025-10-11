7094e4d19f47570da9f503cef8602be049761988
gordon <hyeungac@gmail.com>
2025-10-11 22:05:06 +0800

    addAndDeleteSessionForAnAthlete

---
+++ src/main/java/seedu/fitnessone/command/DeleteSessionCommand.java
@@ -0,0 +1,39 @@
+package seedu.fitnessone.command;
+
+import seedu.fitnessone.controller.Coach;
+import seedu.fitnessone.exception.InvalidCommandException;
+import seedu.fitnessone.ui.Ui;
+
+public class DeleteSessionCommand implements Command {
+    private final String athleteName;
+    private final int sessionID;
+
+    /**
+     * command constructor.
+     * @param athleteName String.
+     * @param sessionID String.
+     */
+    public DeleteSessionCommand(String athleteName, int sessionID) {
+        this.athleteName = athleteName;
+        this.sessionID = sessionID;
+    }
+
+    /**
+     * Executes the command to delete session for a certain athlete and display the message.
+     * @param coachController The task list.
+     * @param view The UI for displaying output.
+     */
+    @Override
+    public void execute(Coach coachController, Ui view) throws InvalidCommandException {
+        coachController.deleteSessionFromAthlete(athleteName, sessionID);
+        view.printWithDivider("Session " + sessionID + " deleted for " + athleteName);
+    }
+    /**
+     * Indicates whether this command should exit the application.
+     * @return false.
+     */
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
