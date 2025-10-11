8d9bf692d1568c50d0075ae75e7e856656940d85
gordon <hyeungac@gmail.com>
2025-10-11 22:03:56 +0800

    addAndDeleteSessionForAnAthlete

--- src/main/java/seedu/fitnessone/controller/Coach.java
+++ src/main/java/seedu/fitnessone/controller/Coach.java
@@ -1,6 +1,8 @@
 package seedu.fitnessone.controller;
 
+import seedu.fitnessone.exception.InvalidCommandException;
 import seedu.fitnessone.model.Athlete;
+import seedu.fitnessone.model.Session;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,5 +15,28 @@ public class Coach {
      */
     public Coach() {
         this.athletes = new ArrayList<>();
+    }
+
+    public void addSessionToAthlete(String athleteName, int sessionID, String sessionTrainingNotes)
+            throws InvalidCommandException {
+        for (Athlete athlete : athletes) {
+            if (athlete.getAthleteName().equals(athleteName)) {
+                Session newSession = new Session(sessionID, sessionTrainingNotes);
+                athlete.addSession(newSession);
+                return;
+            }
+        }
+        throw new InvalidCommandException("Athlete not found: " + athleteName);
+    }
+
+    public void deleteSessionFromAthlete(String athleteName, int sessionID) throws InvalidCommandException {
+
+        for (Athlete athlete : athletes) {
+            if (athlete.getAthleteName().equals(athleteName)) {
+                athlete.removeSession(sessionID);
+                return;
+            }
+        }
+        throw new InvalidCommandException("Athlete not found: " + athleteName);
     }
 }
--- src/main/java/seedu/fitnessone/model/Athlete.java
+++ src/main/java/seedu/fitnessone/model/Athlete.java
@@ -1,6 +1,9 @@
 package seedu.fitnessone.model;
 
+import seedu.fitnessone.exception.InvalidCommandException;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /*
  * All athletes will have 0001. 0002 .... auto created.
@@ -30,6 +33,21 @@ public class Athlete {
         sessions.add(session);
     }
 
+    public void removeSession(int sessionID) throws InvalidCommandException {
+        Iterator<Session> iterator = sessions.iterator();
+        boolean found = false;
+        while (iterator.hasNext()) {
+            Session session = iterator.next();
+            if (session.getSessionId() == sessionID) {
+                iterator.remove();
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            throw new InvalidCommandException("Session not found");
+        }
+    }
 
     public ArrayList<Session> getSessions() {
         return sessions;
