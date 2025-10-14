23c96889b9c49a91c2a4d8d1389a63678532eabc
Ma Zhiheng <e1671904@u.nus.edu>
2025-10-14 18:39:46 +0800

    copy the latest changes to the tp

--- src/main/java/seedu/fitnessone/ui/Parser.java
+++ src/main/java/seedu/fitnessone/ui/Parser.java
@@ -34,62 +34,62 @@ public class Parser {
         int sessionId;
         String trainingNotes;
         switch (commandWord) {
-        case "bye":
-            return new ExitCommand();
-        case "/newSession":
-            try{
-                info = trimmedInput.split("\\s+", 2)[1];
-                parts = info.split(" (?=\\d)", 2);
-                athleteName = parts[0];
-                sessionInfo = parts[1].split("\\s+", 2);
-                sessionId = Integer.parseInt(sessionInfo[0]);
-                trainingNotes = sessionInfo[1];
-                return new NewSessionCommand(athleteName, sessionId, trainingNotes);
-            } catch (ArrayIndexOutOfBoundsException | NullPointerException | NumberFormatException e) {
-                throw new InvalidCommandException("oops, seems like you forgot smth?\n" +
-                        "the correct format is\n" +
-                        "/newSession <Athlete Name> <Session ID> <Description>");
-            }
+            case "bye":
+                return new ExitCommand();
+            case "/newSession":
+                try{
+                    info = trimmedInput.split("\\s+", 2)[1];
+                    parts = info.split(" (?=\\d)", 2);
+                    athleteName = parts[0];
+                    sessionInfo = parts[1].split("\\s+", 2);
+                    sessionId = Integer.parseInt(sessionInfo[0]);
+                    trainingNotes = sessionInfo[1];
+                    return new NewSessionCommand(athleteName, sessionId, trainingNotes);
+                } catch (ArrayIndexOutOfBoundsException | NullPointerException | NumberFormatException e) {
+                    throw new InvalidCommandException("oops, seems like you forgot smth?\n" +
+                            "the correct format is\n" +
+                            "/newSession <Athlete Name> <Session ID> <Description>");
+                }
 
-        case "/deleteSession":
-            try{
-                info = trimmedInput.split("\\s+", 2)[1];
-                parts = info.split(" (?=\\d)", 2);
-                athleteName = parts[0];
-                sessionId = Integer.parseInt(parts[1]);
-                return new DeleteSessionCommand(athleteName, sessionId);
-            } catch (ArrayIndexOutOfBoundsException | NullPointerException | NumberFormatException e) {
-                throw new InvalidCommandException("oops, seems like you forgot smth?\n" +
-                        "the correct format is\n" +
-                        "/deleteSession <Athlete Name> <Session ID>");
-            }
-
-
-        case "newAthlete":
-            return new NewAthleteCommand(trimmedInput);
+            case "/deleteSession":
+                try{
+                    info = trimmedInput.split("\\s+", 2)[1];
+                    parts = info.split(" (?=\\d)", 2);
+                    athleteName = parts[0];
+                    sessionId = Integer.parseInt(parts[1]);
+                    return new DeleteSessionCommand(athleteName, sessionId);
+                } catch (ArrayIndexOutOfBoundsException | NullPointerException | NumberFormatException e) {
+                    throw new InvalidCommandException("oops, seems like you forgot smth?\n" +
+                            "the correct format is\n" +
+                            "/deleteSession <Athlete Name> <Session ID>");
+                }
 
 
-        case "listAthlete":
-            return new ListAthleteCommand();
+            case "newAthlete":
+                return new NewAthleteCommand(trimmedInput);
 
 
-        case "delAthlete":
-            return new DeleteAthleteCommand(trimmedInput);
+            case "listAthlete":
+                return new ListAthleteCommand();
 
-            /*
-            * Add a Training Note to a Session `/trainingNotes <Athlete Name> <Session ID> <Notes>`
-            */
-        case "/trainingNotes":
-            return new AddTrainingNotes(trimmedInput);
+
+            case "delAthlete":
+                return new DeleteAthleteCommand(trimmedInput);
 
             /*
-            * * Mark a Session as Completed `/complete <Athlete Name> <Session ID>`
-            */
-        case "/complete":
-            return new CompleteSession(trimmedInput);
+             * Add a Training Note to a Session `/trainingNotes <Athlete Name> <Session ID> <Notes>`
+             */
+            case "/trainingNotes":
+                return new AddTrainingNotes(trimmedInput);
 
-        default:
-            throw new InvalidCommandException("input keyword not found");
+            /*
+             * * Mark a Session as Completed `/complete <Athlete Name> <Session ID>`
+             */
+            case "/complete":
+                return new CompleteSession(trimmedInput);
+
+            default:
+                throw new InvalidCommandException("input keyword not found");
         }
     }
 }
